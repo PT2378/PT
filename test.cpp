fb591fb6d6ba5327ca7883de147810ff2e352db3
pt2378 <ptpt2378@gmail.com>
2024-06-06 11:55:44 +0630

    testing

---
+++ test.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int a1,a2,a3;
+    cout<<"enter value of two angle\n";
+    cin>>a1>>a2;
+    cout<<"enter value of one angle\n";
+    cin>>a2>>a3;
+    a=180-(a1+a2);
+    cout<<"Value of angle 3 = "<<a3<<endl;
+}
